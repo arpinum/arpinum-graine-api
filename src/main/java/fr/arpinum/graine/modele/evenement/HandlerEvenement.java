c460806bb5060ed6e4f9c0858338b23b602429c4
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2014-04-15 19:22:01 +0200

    template method pour le handler d'evenement

--- src/main/java/fr/arpinum/graine/modele/evenement/HandlerEvenement.java
+++ src/main/java/fr/arpinum/graine/modele/evenement/HandlerEvenement.java
@@ -5,4 +5,12 @@ import fr.arpinum.graine.infrastructure.bus.HandlerMessage;
 
 @SuppressWarnings("UnusedDeclaration")
 public interface HandlerEvenement<TEvenement extends Evenement> extends HandlerMessage<TEvenement, Void> {
+
+    @Override
+    default Void execute(TEvenement evenement) {
+        executeEvenement(evenement);
+        return null;
+    }
+
+    void executeEvenement(TEvenement evenement);
 }
--- src/main/java/votreapplication/recherche/construction/SurFauxEvenement.java
+++ src/main/java/votreapplication/recherche/construction/SurFauxEvenement.java
@@ -6,7 +6,7 @@ import votreapplication.modele.FauxEvenement;
 public class SurFauxEvenement implements HandlerEvenement<FauxEvenement> {
 
     @Override
-    public Void execute(FauxEvenement fauxEvenement) {
-        return null;
+    public void executeEvenement(FauxEvenement evenement) {
+
     }
 }
