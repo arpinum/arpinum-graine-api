0ce74714ee78d0e853871454aa2c8193c97eb004
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2014-04-19 08:45:26 +0200

    oubli sur les racines de base

--- src/main/java/fr/arpinum/graine/modele/RacineDeBase.java
+++ src/main/java/fr/arpinum/graine/modele/RacineDeBase.java
@@ -2,4 +2,11 @@ package fr.arpinum.graine.modele;
 
 @SuppressWarnings("UnusedDeclaration")
 public abstract class RacineDeBase<TId> extends EntiteDeBase<TId> implements Racine<TId> {
+
+    protected RacineDeBase() {
+    }
+
+    protected RacineDeBase(TId tId) {
+        super(tId);
+    }
 }
--- src/main/java/fr/arpinum/graine/modele/RacineDeBaseAvecUuid.java
+++ src/main/java/fr/arpinum/graine/modele/RacineDeBaseAvecUuid.java
@@ -1,5 +1,11 @@
 package fr.arpinum.graine.modele;
 
+import java.util.UUID;
+
 @SuppressWarnings("UnusedDeclaration")
-public abstract class RacineDeBaseAvecUuid implements RacineAvecUuid {
+public abstract class RacineDeBaseAvecUuid extends RacineDeBase<UUID> implements RacineAvecUuid {
+
+    protected RacineDeBaseAvecUuid() {
+        super(UUID.randomUUID());
+    }
 }
