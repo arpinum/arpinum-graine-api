eee925a0bb32a0405571033e4870b6cc879da492
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2014-04-21 14:26:36 +0200

    toujours un oubli sur les génériques

--- src/test/groovy/fr/arpinum/graine/infrastructure/persistance/memoire/EntrepotMemoireAvecUuid.java
+++ src/test/groovy/fr/arpinum/graine/infrastructure/persistance/memoire/EntrepotMemoireAvecUuid.java
@@ -1,9 +1,10 @@
 package fr.arpinum.graine.infrastructure.persistance.memoire;
 
-import fr.arpinum.graine.modele.Racine;
+import fr.arpinum.graine.modele.EntrepotAvecUuid;
+import fr.arpinum.graine.modele.RacineAvecUuid;
 
 import java.util.UUID;
 
 @SuppressWarnings("UnusedDeclaration")
-public class EntrepotMemoireAvecUuid<TRacine extends Racine<UUID>> extends EntrepotMemoire<UUID, TRacine> {
+public class EntrepotMemoireAvecUuid<TRacine extends RacineAvecUuid> extends EntrepotMemoire<UUID, TRacine> implements EntrepotAvecUuid<TRacine> {
 }
