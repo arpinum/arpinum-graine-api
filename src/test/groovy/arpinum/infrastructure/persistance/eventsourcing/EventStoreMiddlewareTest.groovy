6f43ea2b90f54a3b0c934e8bbfe831b576303522
Jean-Baptiste Dusseaut <jbdusseaut@arpinum.fr>
2017-06-07 23:46:36 +0200

    red tests

--- src/test/groovy/arpinum/infrastructure/persistance/eventsourcing/EventStoreMiddlewareTest.groovy
+++ src/test/groovy/arpinum/infrastructure/persistance/eventsourcing/EventStoreMiddlewareTest.groovy
@@ -3,26 +3,28 @@ package arpinum.infrastructure.persistance.eventsourcing
 import arpinum.command.Command
 import arpinum.ddd.event.Event
 import arpinum.ddd.event.EventStore
+import com.google.common.util.concurrent.MoreExecutors
 import io.vavr.Tuple
 import io.vavr.collection.List
+import io.vavr.concurrent.Future
 import spock.lang.Specification
 
 class EventStoreMiddlewareTest extends Specification {
 
     def eventStore = Mock(EventStore)
 
-    def middleware = new EventStoreMiddleware(eventStore)
+    def middleware = new EventStoreMiddleware(eventStore, MoreExecutors.directExecutor())
 
     def "it calls next and saves results"() {
         given:
         def command = {} as Command<String>
-        def event = new Event<String>(){
+        def event = new Event<String>() {
 
         }
         def result = Tuple.of("hello", List.of(event))
 
         when:
-        def value = middleware.intercept(command, { result })
+        def value = middleware.intercept(command, { Future.successful(result) })
 
         then:
         1 * eventStore.save(List.of(event))
